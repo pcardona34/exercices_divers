f8a964fab2bfcb30b2f91e1979a3013b398d904c
pcardona34 <pcardona34@gmail.com>
2019-11-11 23:13:16 +0100

    Ajout d'une activité

---
+++ troisieme/grille32/data.js
@@ -0,0 +1,24 @@
+var oygCrosswordPuzzle = new oyCrosswordPuzzle (
+"2713740989",
+"./JCE",
+"/a/a",
+"Mots croisés",
+"Lexique de la dictée 32 : le tourisme",
+[
+new oyCrosswordClue(5,"Bien rempli.","BONDE","17e61af182fe1109135aad4fa264b0b7",1,12,0)
+,new oyCrosswordClue(5,"D'apparence luxueuse.","COSSU","1ab9713adb62bb8f32558b729f227c97",1,2,0)
+,new oyCrosswordClue(9,"Qui est constitué de choses ou de personnes d'origines différentes.","DISPARATE","d9efea544773965c4473cf4a9c16502c",1,8,0)
+,new oyCrosswordClue(9,"En particulier.","NOTAMMENT","7d79976ba4035ad401cbbc89d8c01e67",1,15,1)
+,new oyCrosswordClue(11,"Digne d'être distingué.","REMARQUABLE","387dce95ce22e31927747bdcf1a3978a",1,17,0)
+,new oyCrosswordClue(12,"Adj. Qui produit un ensemble sonore désagréable.","CACOPHONIQUE","b37b67d5d4c3fa1307f31d277561937c",1,5,2)
+,new oyCrosswordClue(7,"Adj. Qui se compose de courbes successives.","SINUEUX","7bc02d03ee98eefadf36b8d272832137",1,21,1)
+,new oyCrosswordClue(12,"Le maire et ses conseillers.","MUNICIPALITE","2b1ce0ea2fdfe0a93a9f343046a90d06",0,1,4)
+,new oyCrosswordClue(8,"Au sud de la France.","PROVENCE","fea34298b7abbf2385348901596411aa",0,10,1)
+,new oyCrosswordClue(4,"Ancienne ville.","CITE","212e7a551081f5e588b4f4aaa607bef9",0,2,0)
+,new oyCrosswordClue(9,"À la croisée des chemins.","CARREFOUR","5d5a85913ded79f29017961157663a6b",0,14,4)
+,new oyCrosswordClue(10,"Qui indique la provenance.","ORIGINAIRE","38302f8f530da6e589786f4786699ae7",0,1,10)
+],
+25,25);
+oygCrosswordPuzzle.publisherName = "Patrick Cardona";
+oygCrosswordPuzzle.leaveGameURL = "./licence.html";
+oygCrosswordPuzzle.canTalkToServer = false;
