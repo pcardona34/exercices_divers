8e1aa7ab35c0c9c1dcf85f3d5b8f1312b83a516b
pcardona34 <pcardona34@gmail.com>
2021-04-08 12:07:30 +0200

    Nouvelle activité en première

---
+++ premiere/PERSONNAGES_MALADE_IMAGINAIRE/Les_personnages_dans_Le_Malade_imaginaire_fichiers/dragdrop.js
@@ -0,0 +1,50 @@
+// D'après le script dom-drag.js
+// www.youngpup.net
+var lequel="";
+var Drag = {
+obj : null,
+
+init : function(o) {
+		if(o){ // ajouté en janvier 2013
+			o.onmousedown = Drag.start;
+			o.onDragEnd = new Function();
+		}
+	},
+
+	start : function(e) {
+		e = Drag.fixE(e);
+		var o = Drag.obj = this;
+		lequel = o.id;
+		o.lastMouseX = e.clientX;
+		o.lastMouseY = e.clientY;
+		document.onmousemove = Drag.drag;
+		document.onmouseup = Drag.end;
+		return false;
+	},
+
+	drag : function(e) {
+		e = Drag.fixE(e);
+		var o = Drag.obj;
+		var newX = parseInt(o.style.left) + e.clientX - o.lastMouseX;
+		var newY = parseInt(o.style.top) + e.clientY - o.lastMouseY;
+		Drag.obj.style.left = newX + "px";
+		Drag.obj.style.top = newY + "px";
+		Drag.obj.lastMouseX = e.clientX;
+		Drag.obj.lastMouseY = e.clientY;
+		return false;
+	},
+
+	end : function(e) {
+		verification(lequel);
+		e = Drag.fixE(e);
+		document.onmousemove = null;
+		document.onmouseup = null;
+		Drag.obj.onDragEnd(e.clientX, e.clientY);
+		Drag.obj = null;
+	},
+
+	fixE : function(e) {
+		if (typeof e == 'undefined') e = window.event;
+		return e;
+	}
+};
